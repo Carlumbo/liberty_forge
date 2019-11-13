5e31f4bca39cd24bc9e5e8a9b40b5d118b3c42c7
carl_palumbo <carlpalumbo1@gmail.com>
2019-11-12 21:59:03 -0800

    genereated user scaffolds

--- liberty_forge-api/db/migrate/20191107203628_create_property_managers.rb
+++
@@ -1,8 +0,0 @@
-class CreatePropertyManagers < ActiveRecord::Migration[6.0]
-  def change
-    create_table :property_managers do |t|
-      t.string :name
-      t.integer :user_id
-    end
-  end
-end
--- liberty_forge-api/db/schema.rb
+++ liberty_forge-api/db/schema.rb
@@ -10,56 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_07_226052) do
-
-  create_table "landlords", force: :cascade do |t|
-    t.string "name"
-    t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "property_managers", force: :cascade do |t|
-    t.string "name"
-    t.integer "user_id"
-  end
-
-  create_table "props", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.string "structure"
-    t.integer "landlord_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "tenants", force: :cascade do |t|
-    t.string "name"
-    t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2019_11_13_040756) do
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "provider"
-    t.string "uid"
-    t.integer "role"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["provider"], name: "index_users_on_provider"
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["uid"], name: "index_users_on_uid"
   end
 
 end
