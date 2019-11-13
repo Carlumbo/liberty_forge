ca5f4c5800dbcd9e072ffe753876ff8d1ef45111
carl_palumbo <carlpalumbo1@gmail.com>
2019-11-12 22:12:59 -0800

    added addtional migrations

--- liberty_forge-api/db/seeds.rb
+++ liberty_forge-api/db/seeds.rb
@@ -5,10 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Layout.create(name: '3 bedroom, 2 Bath')
-Layout.create(name: '3 bedroom, 3 bath')
-Layout.create(name: '4 bedroom, 3 bath')
-Layout.create(name: '4 bedroom, 4 bath') 
-Layout.create(name: '5 bedroom, 4 bath') 
-Layout.create(name: '5 bedroom, 5 bath') 
-Layout.create(name: '6 bedroom, 6 bath')
